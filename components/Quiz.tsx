8b8595d2c0bc4fd711bdfebe034e958aa0d6d5ee
rjoshf <joshwalters049@gmail.com>
2024-01-29 21:41:36 +0000

    Added shuffling logic to quiz answers

--- components/Quiz.tsx
+++ components/Quiz.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import {useState} from 'react'
+import { useState, useEffect } from 'react'
 
 import QUESTIONS from './questions.js'
 
@@ -8,20 +8,46 @@ import styles from './Quiz.module.css'
 
 
 export default function Quiz() {
+    //array containing the entered answers by the user.
     const [userAnswers, setUserAnswers] = useState<string[]>([]);
-
+    const [shuffledAnswers, setShuffledAnswers] = useState<string[]>([]);
     const activeQuestionIndex = userAnswers.length;
 
+    //checking if quiz is over
+    const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
+    //function to append the answer to the userAnswers array
     function handleSelectAnswer(selectedAnswer: string) {
         setUserAnswers(prevState => [...prevState, selectedAnswer]);
     }
 
+    //useEffect required to stop hydration error as we are shuffling the answers causing a mismatch between React tree that was pre-rendered and server side value
+    useEffect(() => {
+        if (!quizIsComplete) {
+            setShuffledAnswers(QUESTIONS[activeQuestionIndex].answers.sort(() => Math.random() - 0.5))
+        }
+    }, [quizIsComplete, activeQuestionIndex])
+
+    if (quizIsComplete) {
+        return (
+            <div>
+                <h2>Completed!</h2>
+            </div>
+        )
+    }
+
+
     return (
-        <div className={styles.question}>
-            <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-            <ul className={styles.answers}>
-                {QUESTIONS[activeQuestionIndex].answers.map(answer => <li key={answer}><button onClick={() => handleSelectAnswer(answer)}>{answer}</button></li>)}
-            </ul>
-        </div>
+        <>
+            {!quizIsComplete && <div className={styles.quiz}>
+                <div className={styles.question}>
+                    <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+                    <ul className={styles.answers}>
+                        {shuffledAnswers.map(answer => <li key={answer}><button onClick={() => handleSelectAnswer(answer)}>{answer}</button></li>)}
+                    </ul>
+                </div>
+            </div>}
+            {quizIsComplete && <h2>Completed!</h2>}
+        </>
     )
 }
