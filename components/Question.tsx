1c3303e4e75f3f0d61709e0f8c07992327668d71
rjoshf <joshwalters049@gmail.com>
2024-01-31 20:13:58 +0000

    Added scoring logic and moved props and state

--- components/Question.tsx
+++ components/Question.tsx
@@ -5,14 +5,55 @@ import styles from './Question.module.css'
 
 import QUESTIONS from './questions.js'
 
-const Question: React.FC<{ answerState: string; handleSelectAnswer: (answer: string) => void; handleSkipAnswer: () => void; userAnswers: string[], questionIndex: number }> = ({ answerState, handleSelectAnswer, handleSkipAnswer, userAnswers, questionIndex }) => {
+import { useState, useCallback } from 'react'
+
+const Question: React.FC<{}> = () => {
+
+    const [answerState, setAnswerState] = useState('')
+    const [userScore, setUserScore] = useState(0);
+    const [userAnswers, setUserAnswers] = useState<string[]>([]);
+
+    const activeQuestionIndex = answerState === '' ? userAnswers.length : userAnswers.length - 1;
+
+    const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
+    const handleSelectAnswer = (selectedAnswer: string) => {
+        setAnswerState('answered');
+        setUserAnswers(prevState => [...prevState, selectedAnswer]);
+
+        setTimeout(() => {
+            if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
+                setAnswerState('correct');
+                setUserScore(prevUserScore => prevUserScore + 1);
+            } else {
+                setAnswerState('wrong');
+            }
+
+            setTimeout(() => {
+                setAnswerState('')
+            }, 2000)
+        }, 1000)
+    }
+
+    const handleSkipAnswer = useCallback(() => {
+        setUserAnswers(prevState => [...prevState, "question skipped"])
+    }, [])
 
     return (
-        <div className={styles.question}>
-            {answerState === '' && <QuestionTimer key={questionIndex} timeout={10000} onTimeout={handleSkipAnswer}></QuestionTimer>}
-            <h2>{QUESTIONS[questionIndex]?.text}</h2>
-            <Answers answers={QUESTIONS[questionIndex]?.answers} answerState={answerState} userAnswers={userAnswers} handleSelectAnswer={handleSelectAnswer}></Answers>
-        </div>
+        <>
+            {!quizIsComplete && <div className={styles.question}>
+                {answerState === '' && <QuestionTimer key={activeQuestionIndex} timeout={10000} onTimeout={handleSkipAnswer}></QuestionTimer>}
+                <h2>{QUESTIONS[activeQuestionIndex]?.text}</h2>
+                <Answers answers={QUESTIONS[activeQuestionIndex]?.answers} answerState={answerState} userAnswers={userAnswers} handleSelectAnswer={handleSelectAnswer}></Answers>
+            </div>}
+            {quizIsComplete && (
+                <>
+                    <h1>{`Mark: ${userScore} out of ${QUESTIONS.length}`}</h1>
+                    <h1>{`${Math.round(userScore / QUESTIONS.length * 100)}%`}</h1>
+                </>
+            )
+            }
+        </>
     )
 }
 
--- components/Quiz.tsx
+++ components/Quiz.tsx
@@ -2,62 +2,13 @@
 
 import Question from './Question'
 
-import { useState, useCallback } from 'react'
-
-import QUESTIONS from './questions.js'
-
 import styles from './Quiz.module.css'
 
 export default function Quiz() {
 
-    const [answerState, setAnswerState] = useState('')
-    //State containing the entered answers by the user.
-    const [userAnswers, setUserAnswers] = useState<string[]>([]);
-
-    const [score, setScore] = useState(0);
-
-    //constant to determine which question is currently active.
-    const activeQuestionIndex = answerState === '' ? userAnswers.length : userAnswers.length - 1;
-
-    //constant to represent if quiz has ended
-    const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
-
-    //function to append the answer to the userAnswers array
-    const handleSelectAnswer = (selectedAnswer: string) => {
-        setAnswerState('answered');
-        setUserAnswers(prevState => [...prevState, selectedAnswer]);
-
-        setTimeout(() => {
-            if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
-                setAnswerState('correct');
-                setScore(prevState => prevState + 1)
-            } else {
-                setAnswerState('wrong');
-            }
-
-            setTimeout(() => {
-                setAnswerState('')
-            }, 2000)
-        }, 1000)
-    }
-
-    //function to handle running out of time - useCallback is to cache this function to stop it over-running, as its a useEffect dependency
-    const handleSkipAnswer = useCallback(() => {
-        setUserAnswers(prevState => [...prevState, "question skipped"])
-    }, [])
-
     return (
-        <>
-            {!quizIsComplete && <div className={styles.quiz}>
-                <Question
-                    questionIndex={activeQuestionIndex}
-                    answerState={answerState}
-                    handleSelectAnswer={handleSelectAnswer}
-                    handleSkipAnswer={handleSkipAnswer}
-                    userAnswers={userAnswers}
-                ></Question>
-            </div>}
-            {quizIsComplete && <h2>{score}</h2>}
-        </>
+        <div className={styles.quiz}>
+            <Question></Question>
+        </div>
     )
 }
