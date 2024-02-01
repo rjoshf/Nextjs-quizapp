69d1dd6792eabb9a6f0a2636f54a7f2b4b49c88b
rjoshf <joshwalters049@gmail.com>
2024-02-01 04:36:47 +0000

    Added local storage to the timer to prevent bugs

--- components/QuestionTimer.tsx
+++ components/QuestionTimer.tsx
@@ -1,38 +1,51 @@
-import { useState, useEffect } from "react";
-
+import React, { useState, useEffect } from "react";
 import styles from './QuestionTimer.module.css'
 
 const QuestionTimer: React.FC<{ timeout: number; onTimeout: () => void; }> = ({ timeout, onTimeout }) => {
-    //state keeping track of the remaining time on the question.
+    // State keeping track of the remaining time on the question.
     const [remainingTime, setRemainingTime] = useState(timeout);
 
-    //Setting the timer for a question and triggering the onTimeout function when time is up.
     useEffect(() => {
-        const timer = setTimeout(onTimeout, timeout);
+        const storedTimeRemaining = localStorage.getItem('remainingTime');
+        if (storedTimeRemaining) {
+            setRemainingTime(JSON.parse(storedTimeRemaining));
+        }
+    }, []);
 
-        //clean up function
+    // Setting the timer for a question and triggering the onTimeout function when time is up.
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            onTimeout();
+            setRemainingTime(timeout); // Reset remaining time on timeout
+        }, remainingTime);
+
+        // Clean up function
         return () => {
             clearTimeout(timer);
-        }
-    }, [timeout, onTimeout])
+        };
+    }, [remainingTime, timeout, onTimeout]);
 
-    //Setting up intervals in which time is deducted from the time left for the question.
+    // Setting up intervals in which time is deducted from the time left for the question.
     useEffect(() => {
-
         const interval = setInterval(() => {
-            setRemainingTime(prevRemainingTime => prevRemainingTime - 50)
+            setRemainingTime(prevRemainingTime => {
+                const newRemainingTime = prevRemainingTime - 50;
+                localStorage.setItem('remainingTime', JSON.stringify(newRemainingTime));
+                return newRemainingTime;
+            });
         }, 50);
 
-        //clean up function
+        // Clean up function
         return () => {
             clearInterval(interval);
+            localStorage.removeItem('remainingTime')
         };
     }, []);
 
-    //jsx displaying the time progress bar on each question.
+    // JSX displaying the time progress bar on each question.
     return (
         <progress className={styles.questiontime} value={remainingTime} max={timeout}></progress>
-    )
+    );
 }
 
 export default QuestionTimer;
