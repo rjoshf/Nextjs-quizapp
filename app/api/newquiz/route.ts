cf34ff9a576234466358f9f4800e1a014edf8891
rjoshf <joshwalters049@gmail.com>
2024-03-04 22:42:09 +0000

    Removed some comments

--- app/api/newquiz/route.ts
+++ app/api/newquiz/route.ts
@@ -2,7 +2,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { MongoClient } from 'mongodb';
 
-// Define the API route handler
 export async function POST(req: NextRequest, res: NextResponse) {
     try {
         const data = await req.json();
@@ -11,32 +10,25 @@ export async function POST(req: NextRequest, res: NextResponse) {
             throw new Error('MONGODB_URI is not defined');
         }
         
-
-        // Connect to MongoDB
         const client = await MongoClient.connect(process.env.MONGODB_URI);
         const db = client.db();
 
-        // Access the Quizzes collection
         const quizzesCollection = db.collection('Quizzes');
 
-        // Insert data into the collection
         await quizzesCollection.insertOne(data);
 
-        // Close the MongoDB connection
         client.close();
 
-        // Return success response
         return NextResponse.json({
             message: 'Quiz created successfully',
         });
     } catch (error) {
         console.error(error);
 
-        // Return error response
         return NextResponse.json({
             message: 'Error creating quiz',
         }, {
-            status: 500, // Internal Server Error
+            status: 500,
         });
     }
 }
