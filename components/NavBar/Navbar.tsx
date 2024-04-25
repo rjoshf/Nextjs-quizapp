7aadf920bc90d32635e78adf2a0ee092ebc95524
rjoshf <joshwalters049@gmail.com>
2024-04-25 18:31:59 +0100

    Added active navbar links

--- components/NavBar/Navbar.tsx
+++ components/NavBar/Navbar.tsx
@@ -1,15 +1,22 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+
 import Link from 'next/link';
 
-import styles from './Navbar.module.css'
+import styles from './Navbar.module.css';
 
 export default function Navbar() {
+
+    const pathname = usePathname();
+
     return (
         <nav className={styles.navbar}>
-            <Link className={styles.navlink} href="/">NextQuiz</Link>
+            <Link className={`${styles.navlink} ${pathname.endsWith('/') ? styles.active : ''}`} href="/">NextQuiz</Link>
             <div className={styles.groupednavlinks}>
-                <Link className={styles.navlink} href="/newquiz">Add Quiz</Link>
-                <Link className={styles.navlink} href="/deletequiz">Delete Quiz</Link>
+                <Link className={`${styles.navlink} ${pathname.startsWith('/newquiz') ? styles.active : ''}`} href="/newquiz">Add Quiz</Link>
+                <Link className={`${styles.navlink} ${pathname.startsWith('/deletequiz') ? styles.active : ''}`} href="/deletequiz">Delete Quiz</Link>
             </div>
         </nav>
-    )
+    );
 }
